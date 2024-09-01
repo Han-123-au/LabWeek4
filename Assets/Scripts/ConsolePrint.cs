28ecc42725766b56ec1d32604a403658361efe5c
Han-123-au <zihan.Liu-3@student.uts.edu.au>
2024-09-01 19:00:20 +0800

    Implemented GameObject property printing in ConsolePrint script

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i = 3;
+
+    void Update()
+    {
+        i++;
+        Debug.Log(transform.name + ":" + i);
+    }
+}
