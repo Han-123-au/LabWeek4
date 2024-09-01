4e543318dc30585cfe51c769062b2a23fc2125f2
Han-123-au <zihan.Liu-3@student.uts.edu.au>
2024-09-01 19:12:46 +0800

    Implemented game object deactivation and component disabling based on conditions

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
 {
-    public Renderer rend; 
+    public Renderer rend;
     private int i = 3;
+    private int randomBlueValue;
+
+    void Start()
+    {
+        randomBlueValue = Random.Range(150, 251); 
+    }
 
     void Update()
     {
         i++;
         Debug.Log(transform.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.tag == "Blue" && i == randomBlueValue)
+        {
+            rend.enabled = false;
+        }
     }
 }
